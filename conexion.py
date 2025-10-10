79ccd545e63cc568f2d1225b813eed03c2b99013
ErickCJS <julonerick1@gmail.com>
2025-10-10 00:44:18 -0500

    errores
    errorres de base de datos y de conexion

---
+++ conexion.py
@@ -0,0 +1,18 @@
+import pymysql
+
+def conectarbd():
+    try:
+        print("conecto")
+        return pymysql.connect(
+            host='preguntas-calculadoramateriales.e.aivencloud.com',
+            user='avnadmin',
+            password='AVNS_LFRg88CLhVu59pB2C0e',
+            db='cuestionario',
+            port=10416,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+    
+    except pymysql.Error as error:
+        
+        return error
+    
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template
-
-
-
+import conexion 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    conexion.conectarbd()
     return "Hola mundo"  # O puedes usar return "Hola Flask"
+
+
+
+
+#Ruta si se cayo el servidor de base de datos
+@app.route('/errorsistema')
+def errorsistema():
+    conexion.conectarbd()
+    return render_template('/Fallas/maestra_error.html') # O puedes usar return "Hola Flask"
 
 if __name__ == '__main__':
     app.run(debug=True)
